a1cee17eff891b965aec75034b5e84fb5e55cd82
Patrick-Molina-157419 <150029044+Patrick-Molina-157419@users.noreply.github.com>
2024-04-17 17:12:10 +0200

    Update codeUtils.py

--- codeUtils.py
+++ codeUtils.py
@@ -15,9 +15,15 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.svm import SVC
 
-
-
 def load_data():
+    """
+    Load Titanic dataset from CSV files.
+
+    Returns:
+    tuple: A tuple containing two DataFrames:
+           - titanic_test (DataFrame): DataFrame containing test data.
+           - titanic_train (DataFrame): DataFrame containing train data.
+    """
     titanic_train = pd.read_csv('data/train.csv')
     titanic_test = pd.read_csv('data/test.csv')
     y_test = pd.read_csv('data/gender_submission.csv')
@@ -25,25 +31,80 @@ def load_data():
     return (titanic_test, titanic_train)
 
 
+
 def drop_unnecessary_columns(table):
+    """
+    Drop unnecessary columns from the DataFrame.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+
+    Returns:
+    None
+    """
     table.drop(columns=['PassengerId', 'Name','Ticket', 'Cabin'], inplace=True)
 
 
+
 def drop_na_rows(table):
+    """
+    Drop rows with missing values from the DataFrame.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+
+    Returns:
+    None
+    """
     for col in table.columns:
         table.dropna(inplace=True)
 
 
+
 def bin_sex_attributes(table):
+    """
+    Bin the 'Sex' attribute into numerical values.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+
+    Returns:
+    None
+    """
     table['Sex'] = table['Sex'].map({'male': 0, 'female': 1})
 
 
+
 def convert_embarked_into_numbers(table):
+    """
+    Convert the 'Embarked' attribute into numerical values.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+
+    Returns:
+    None
+    """
     label_encoder = LabelEncoder()
     table['Embarked'] = label_encoder.fit_transform(table['Embarked'])
 
 
+
 def get_data_for_classification(train_data, test_data):
+    """
+    Prepare data for classification by separating features and labels.
+
+    Parameters:
+    train_data (DataFrame): DataFrame containing training data.
+    test_data (DataFrame): DataFrame containing test data.
+
+    Returns:
+    tuple: A tuple containing four elements:
+           - x_train (DataFrame): Features for training.
+           - y_train (Series): Labels for training.
+           - x_test (DataFrame): Features for testing.
+           - y_test (Series): Labels for testing.
+    """
     y_train = train_data['Survived']
     y_test = test_data['Survived']
     x_train = train_data.drop(columns=['Survived'])
@@ -51,52 +112,121 @@ def get_data_for_classification(train_data, test_data):
     return (x_train, y_train, x_test, y_test)
 
 
+
 def test_accuracy_svc(x_train, y_train, x_test, y_test):
+    """
+    Test accuracy of Support Vector Classifier.
+
+    Parameters:
+    x_train (DataFrame): Features for training.
+    y_train (Series): Labels for training.
+    x_test (DataFrame): Features for testing.
+    y_test (Series): Labels for testing.
+
+    Returns:
+    None
+    """
     classifier = SVC()
     classifier.fit(x_train, y_train)
     y_pred = classifier.predict(x_test)
     print(classification_report(y_test, y_pred))
     accuracy = accuracy_score(y_test, y_pred)
-
     print(f'The accuracy of the classifier is: {accuracy}')
 
+
+
 def test_accuracy_random_forrest(x_train, y_train, x_test, y_test):
+    """
+    Test accuracy of Random Forest Classifier.
+
+    Parameters:
+    x_train (DataFrame): Features for training.
+    y_train (Series): Labels for training.
+    x_test (DataFrame): Features for testing.
+    y_test (Series): Labels for testing.
+
+    Returns:
+    None
+    """
     classifier = RandomForestClassifier()
     classifier.fit(x_train, y_train)
     y_pred = classifier.predict(x_test)
     print(classification_report(y_test, y_pred))
     accuracy = accuracy_score(y_test, y_pred)
+    print(f'The accuracy of the classifier is: {accuracy}')
 
-    print(f'The accuracy of the classifier  is: {accuracy}')
 
 
 def test_accuracy_logistic_regression(x_train, y_train, x_test, y_test):
+    """
+    Test accuracy of Logistic Regression Classifier.
+
+    Parameters:
+    x_train (DataFrame): Features for training.
+    y_train (Series): Labels for training.
+    x_test (DataFrame): Features for testing.
+    y_test (Series): Labels for testing.
+
+    Returns:
+    None
+    """
     classifier = LogisticRegression()
     classifier.fit(x_train, y_train)
     y_pred = classifier.predict(x_test)
     print(classification_report(y_test, y_pred))
     accuracy = accuracy_score(y_test, y_pred)
-
     print(f'The accuracy of the classifier is: {accuracy}')
 
+
+
 def normalize(table, numerical_cols):
+    """
+    Normalize numerical columns in the DataFrame.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+    numerical_cols (list): List of numerical column names.
+
+    Returns:
+    None
+    """
     norm_scaler = MinMaxScaler()
     table[numerical_cols] = norm_scaler.fit_transform(table[numerical_cols])
 
 
+
 def standardize(table, numerical_cols):
+    """
+    Standardize numerical columns in the DataFrame.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+    numerical_cols (list): List of numerical column names.
+
+    Returns:
+    None
+    """
     scaler_std = StandardScaler()
     table[numerical_cols] = scaler_std.fit_transform(table[numerical_cols])
 
 
-def  features_selection_logistic_regression(table_train):
+
+def features_selection_logistic_regression(table_train):
+    """
+    Perform feature selection for Logistic Regression Classifier.
+
+    Parameters:
+    table_train (DataFrame): DataFrame containing training data.
+
+    Returns:
+    list: List of selected features.
+    """
     X_train = table_train.drop(columns=['Survived'])
     y_train = table_train['Survived']
     number_of_features = range(1, len(X_train.columns) + 1)
     estimator = LogisticRegression()
     scores = []
     selected_features_list = []
-
 
     for i in number_of_features:
         rfe = RFE(estimator, n_features_to_select=i)
@@ -123,7 +253,17 @@ def  features_selection_logistic_regression(table_train):
     return best_selected_features
 
 
-def  features_selection_random_forrest(table_train):
+
+def features_selection_random_forrest(table_train):
+    """
+    Perform feature selection for Random Forest Classifier.
+
+    Parameters:
+    table_train (DataFrame): DataFrame containing training data.
+
+    Returns:
+    list: List of selected features.
+    """
     X_train = table_train.drop(columns=['Survived'])
     y_train = table_train['Survived']
 
@@ -132,7 +272,6 @@ def  features_selection_random_forrest(table_train):
                 cv=StratifiedKFold(10), scoring='accuracy')
 
     rfecv.fit(X_train, y_train)
-
 
     plt.figure(figsize=(10, 6))
     plt.xlabel("Number of features")
@@ -148,16 +287,37 @@ def  features_selection_random_forrest(table_train):
     return selected_features
 
 
+
 def extract_features(table, selected_features):
+    """
+    Extract selected features from the DataFrame.
+
+    Parameters:
+    table (DataFrame): DataFrame containing the data.
+    selected_features (list): List of selected feature names.
+
+    Returns:
+    DataFrame: DataFrame containing only the selected features.
+    """
     features = list(selected_features)
     features.append("Survived")
     return table[features]
-# X_test.drop(columns=['SibSp', 'Parch', 'Embarked', "Survived"],inplace=True)
 
-# X_test
 
 
 def pca_transform(train_data, test_data):
+    """
+    Perform Principal Component Analysis (PCA) transformation.
+
+    Parameters:
+    train_data (DataFrame): DataFrame containing training data.
+    test_data (DataFrame): DataFrame containing test data.
+
+    Returns:
+    tuple: A tuple containing two DataFrames:
+           - X_test_pca (DataFrame): Transformed test data.
+           - X_train_pca (DataFrame): Transformed training data.
+    """
     x_train = train_data.drop(columns=['Survived'])
     x_test = test_data.drop(columns=['Survived'])
     pca = PCA(n_components=4)
